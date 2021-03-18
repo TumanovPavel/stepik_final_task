bee964ec6fcde401d342e7f98666bca72df9c989
Туманов Павел Валерьевич <p.tumanov@skbkontur.ru>
2021-03-18 12:11:09 +0600

    Исправил тест

--- test_main_page.py
+++ test_main_page.py
@@ -3,7 +3,10 @@ import time
 import math
 import pytest
 
-def test_add_to_basket_button_is_present(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
     assert browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket") != None, \
         "Кнока 'Добавить в корзину' не найдена на странице"
